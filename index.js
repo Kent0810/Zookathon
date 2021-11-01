cb2cb5f47fc89736742158afb9935be087db8f75
Kent0810 <buianhkent123@gmail.com>
2021-11-01 22:28:42 +0700

    second commit

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ x.addEventListener("click",()=>{
 
     sidebar.style.marginRight ="-245px";
 })
+
+//reveal
+window.addEventListener('scroll',reveal);
+
+
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+    for(var i =  0;i<reveals.length;i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint =150;
+
+        if(revealTop<windowHeight-revealPoint){
+            reveals[i].classList.add('active');
+        }
+        //optional
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
