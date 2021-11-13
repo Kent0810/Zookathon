fa38a352fce9c12e08c5cd169ce614f2899f29b9
Kent0810 <buianhkent123@gmail.com>
2021-11-13 11:43:38 +0700

    Kent Final Commit

--- JS/report.js
+++ JS/report.js
@@ -19,12 +19,15 @@ InitialSlideIn();
 
 const myForm =document.getElementById('myForm');
 const inpFile =document.getElementById('inpFile');
+const fileName =document.querySelector(".fileName");
 
+inpFile.addEventListener('change',(e)=>{
+   fileName.innerHTML = e.target.files[0].name;
+})
 myForm.addEventListener('submit',e=>{
     e.preventDefault();
     const endpoint = "upload.php";
     const formData =new FormData();
-    // console.log(inpFile.files)
     formData.append("inpFile",inpFile.files[0]);  
     fetch(endpoint,{
         method: "POST",
@@ -35,9 +38,10 @@ myForm.addEventListener('submit',e=>{
 
 });//stop redirecting / refrreshing
 
-const btn = document.querySelector(".fa-upload");
+const btn = document.querySelector(".fa-cloud-upload-alt");
 btn.addEventListener("click",()=>{
     setTimeout(()=>{
+        fileName.innerHTML = ""
         alert("Đã Gửi !")
     },500)
 })
